805ec200bd43155630cc08fed77a02dca1bd7d58
walid abou ali <walid.abou.ali@ibm.com>
2021-01-23 23:40:09 +0400

    init

---
+++ words.js
@@ -0,0 +1,33 @@
+var words = [
+    {
+        "text": 'Paris',
+        "team": 'team2',
+    },
+    {
+        "text": 'Word 2',
+        "team": 'team2',
+    },
+    {
+        "text": 'Word 3',
+        "team": 'team1',
+    },
+    {
+        "text": 'Word 4',
+        "team": 'team1',
+    },
+
+    {
+        "text": 'Word 5',
+        "team": 'team1',
+    },
+    {
+        "text": 'Word 6',
+        "team": 'team1',
+    },
+    
+    
+]
+
+
+var team1 = "Blue Team";
+var team2 = "Red Team";
